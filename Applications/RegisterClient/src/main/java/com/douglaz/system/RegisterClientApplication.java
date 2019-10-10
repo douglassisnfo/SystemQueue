18ab034f681cd106ff1b57eaf9e01262da164220
Douglas <douglas.sisnfo@gmail.com>
2019-10-10 17:12:58 -0300

    Configuration of informations to sistem

--- Applications/RegisterClient/src/main/java/com/douglaz/system/RegisterClientApplication.java
+++ Applications/RegisterClient/src/main/java/com/douglaz/system/RegisterClientApplication.java
@@ -12,5 +12,4 @@ public class RegisterClientApplication extends SpringBootServletInitializer{
 	public static void main(String[] args) {
 		SpringApplication.run(RegisterClientApplication.class, args);
 	}
-
 }
--- Applications/RegisterClient/src/main/java/com/douglaz/system/controller/Hello.java
+++ Applications/RegisterClient/src/main/java/com/douglaz/system/controller/Hello.java
@@ -2,19 +2,20 @@ package com.douglaz.system.controller;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Hello {
 	
-	@GetMapping("/")
+	org.slf4j.Logger logger = LoggerFactory.getLogger(Hello.class);
+	 
+	@GetMapping("/hello")
 	public String get() throws UnknownHostException {
 		
-		Logger.getLogger(Hello.class.getName()).log(Level.SEVERE, "Acesso aos dados  - SERVER {0}", InetAddress.getLocalHost().getHostAddress());
+		logger.trace("Acesso aos dados  - SERVER {0}", InetAddress.getLocalHost().getHostAddress());
 		return "get data server " + InetAddress.getLocalHost().getHostAddress();
 	}
 
